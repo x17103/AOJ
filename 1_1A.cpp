56d10ff64362ad3363a52aa34aea0f88a3520068
x17103xx <>
2019-06-03 11:46:19 +0900

    1-1A

---
+++ 1_1A.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Hello Warld
+//
+//  Created by x17103xx on 2019/06/03.
+//  Copyright © 2019年 AIT. All rights reserved.
+//
+
+#include <iostream>
+int main(int argc, const char * argv[]) {
+    printf("Hello World\n");
+    return 0;
+}
